2fe76c2d70aba334ce3da277bda67f090f11149d
GuiMarcelino <guilhermemarcelino_91@outlook.com>
2021-08-17 20:55:13 -0300

    iteracoes

---
+++ condicionais/iteracao/for.rb
@@ -0,0 +1,14 @@
+frutas = ['maçã', 'uva', 'morango']
+
+for fruta in frutas
+  puts fruta
+end
+
+# contar itens da lista
+
+lista = ['feijão', 'arroz', 'carne', 'verdura' ]
+contador = 0
+for item in lista
+  puts "item da lista: #{item}"
+  puts "Número do item #{contador+=1}"
+  end
