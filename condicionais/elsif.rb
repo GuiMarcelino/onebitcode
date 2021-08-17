097813839661c361808bd7f81220201f14b6272b
GuiMarcelino <guilhermemarcelino_91@outlook.com>
2021-08-17 18:51:19 -0300

    condicionais

---
+++ condicionais/elsif.rb
@@ -0,0 +1,13 @@
+fruta = 'pera'
+
+if fruta == 'pera'
+  resp = "adoro pera"
+elsif fruta == 'banana'
+  resp = "não gosto de banana"
+elsif fruta == 'maçã'
+  resp 'minha fruta preferida!!!'
+else
+  resp "Não gosto dessa fruta"
+end
+
+puts (resp)
---
+++ condicionais/unless.rb
@@ -0,0 +1,9 @@
+status = 'fechado'
+
+unless status == 'executa quando a condição for false'
+  resp = 'está fechado'
+else
+  resp 'está aberto' 
+end
+
+puts (status)
