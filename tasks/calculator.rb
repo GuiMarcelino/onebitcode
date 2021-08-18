433644daea805afe698393efd44e511c05773762
GuiMarcelino <guilhermemarcelino_91@outlook.com>
2021-08-18 18:17:17 -0300

    finished task calculator

--- tasks/calculator.rb
+++ tasks/calculator.rb
@@ -1,20 +1,58 @@
 option = ''
 
 loop do
-  puts "Calculadora"
+  puts ""
+  puts "Inicializando Calculadora"
   puts "Escolha uma das opções abaixo"
   puts "Para adição         -> [+]"
   puts "Para subtração      -> [-]"
   puts "Para multiplicação  -> [*]"
   puts "Para divisão        -> [/]"
   puts "Para sair digite    -> [!]"
+  print "Opção: "
   
-  option = gets.chomp.to_i
+  option = gets.chomp
 
-  if option == 1
+  if option == '+'
     puts "Digite um número: "
     number_1 = gets.chomp.to_f
     puts "Digite outro número: "
     number_2 = gets.chomp.to_f
-    result = number_1 
-    puts (result)
+    result = number_1 + number_2
+    puts "A soma entre #{number_1} e #{number_2} = #{result.round 2}"
+  
+  elsif option == '-'
+    puts "Digite um número: "
+    number_1 = gets.chomp.to_f
+    puts "Digite outro número: "
+    number_2 = gets.chomp.to_f
+    result = number_1 - number_2
+    puts "A subtração entre #{number_1} e #{number_2} = #{result.round 2}"
+
+  elsif option == '*'
+    puts "Digite um número: "
+    number_1 = gets.chomp.to_f
+    puts "Digite outro número: "
+    number_2 = gets.chomp.to_f
+    result = number_1 * number_2
+    puts "A multiplicação entre #{number_1} e #{number_2} = #{result.round 2}"
+
+  elsif option == '/'
+    puts "Digite um número: "
+    number_1 = gets.chomp.to_f
+    puts "Digite outro número: "
+    number_2 = gets.chomp.to_f
+    result = number_1 / number_2
+    puts "A divisão entre #{number_1} e #{number_2} = #{result.round 2}"
+
+  elsif option == '!'
+    puts "Finalizando programa"
+    break
+
+  else
+    puts "Opção inválida , escolha uma das opções sugeridas "
+    puts "Aguarde alguns segundos , programa inicializando novamente!"
+    sleep(2)
+  end
+end
+
