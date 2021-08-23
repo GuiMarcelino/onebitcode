eac47db7a769280bcfdb663089f1a553a4c134ff
GuiMarcelino <guilhermemarcelino_91@outlook.com>
2021-08-22 21:44:23 -0300

    create metodos

---
+++ metodos/metodo.rb
@@ -0,0 +1,9 @@
+def talk(first_name, last_name) # parametros uqe vamos receber na chamada
+  puts "Olá #{first_name} #{last_name}, como você está?"
+end
+
+first_name1 = "Guilherme" # criando variavel e passando um valor 
+last_name2 = "Marcelino"
+
+talk(first_name1, last_name2) # aqui estamos passando como parametro as variaveis que criamos , notamos que não precisa ter o mesmo nome,
+# dos parametros da função .
